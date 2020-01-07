8470ba0cb84a92cd6c467ac58d7698789ce1b2e0
Dibbydib <59593041+Dibbydib@users.noreply.github.com>
2020-01-07 15:49:54 +1030

    Create Reach.lua

---
+++ Reach.lua
@@ -0,0 +1,4 @@
+-- Sporked from CodeCorp.
+game.Players.LocalPlayer.Backpack["[Knife]"].Handle.Size = Vector3.new(100, 100, 100)
+game.Players.LocalPlayer.Backpack["[Pitchfork]"].Handle.Size = Vector3.new(100, 100, 100)
+game.Players.LocalPlayer.Backpack["[Bat]"].Handle.Size = Vector3.new(100, 100, 100)
