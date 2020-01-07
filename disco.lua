da3d695c4471854c4924e229d80fb6148ec44902
Dibbydib <59593041+Dibbydib@users.noreply.github.com>
2020-01-07 15:35:38 +1030

    Create disco.lua

---
+++ disco.lua
@@ -0,0 +1,7 @@
+-- Created by Dibbydib (roblox/dibby_dib)
+while true do
+game.Players.LocalPlayer.UpperTorso = BrickColor.new(math.random(), math.random(), math.random())
+wait(0.1) 
+game.Players.LocalPlayer.UpperTorso = BrickColor.new(math.random(), math.random(), math.random())
+wait(0.1) 
+end
