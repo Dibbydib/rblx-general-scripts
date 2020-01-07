dce1bf58dc3e4289935c3fa514ea13161cb6eaed
Dibbydib <59593041+Dibbydib@users.noreply.github.com>
2020-01-07 16:19:32 +1030

    add notice

--- disco.lua
+++ disco.lua
@@ -1,4 +1,5 @@
 -- Created by Dibbydib (roblox/dibby_dib)
+-- WARNING!! This one is untested. Excecute with caution.
 while true do
 game.Players.LocalPlayer.UpperTorso = BrickColor.new(math.random(), math.random(), math.random())
 wait(0.1) 
