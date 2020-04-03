0a617d65eed3dc9f2223bf2b780c0d38c87b21e6
Dibbydib <59593041+Dibbydib@users.noreply.github.com>
2020-04-03 11:49:39 +1030

    custom item support for dummies

--- dahood-Reach.lua
+++ dahood-Reach.lua
@@ -1,4 +1,6 @@
 -- Sporked from CodeCorp.
+-- Set "["[Name]"] item to a custom item's name to enable reach on that item
 game.Players.LocalPlayer.Backpack["[Knife]"].Handle.Size = Vector3.new(100, 100, 100)
 game.Players.LocalPlayer.Backpack["[Pitchfork]"].Handle.Size = Vector3.new(100, 100, 100)
 game.Players.LocalPlayer.Backpack["[Bat]"].Handle.Size = Vector3.new(100, 100, 100)
+game.Players.LocalPlayer.Backpack["[Name]"].Handle.Size = Vector3.new(100, 100, 100)
